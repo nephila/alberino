5b854c9dd640aef5a9ec20d2a35fe8d17e3770f0
Andrea Stagi <stagi.andrea@gmail.com>
2014-12-22 19:16:44 +0100

    Fix indentation

--- Animation.h
+++ Animation.h
@@ -4,20 +4,20 @@
 #include "Animation.h"
 
 class Animation {
-     private:
-         unsigned int repetitions;
-         unsigned int totalLeds;
-     public:
-         virtual void drawFrame() = 0;
-         Animation* setRepetitions(unsigned int repetitions) {this->repetitions = repetitions; return this;};
-         unsigned int getRepetitions() {return this->repetitions;};
-         Animation* setTotalLeds(unsigned int totalLeds) {this->totalLeds = totalLeds; return this;};
-         unsigned int getTotalLeds() {return this->totalLeds;};
-         void animate() {
-             for (int i = 0 ; i < getRepetitions() ; i++) {
-                 drawFrame();
-             }
-         }
+    private:
+        unsigned int repetitions;
+        unsigned int totalLeds;
+    public:
+        virtual void drawFrame() = 0;
+        Animation* setRepetitions(unsigned int repetitions) {this->repetitions = repetitions; return this;};
+        unsigned int getRepetitions() {return this->repetitions;};
+        Animation* setTotalLeds(unsigned int totalLeds) {this->totalLeds = totalLeds; return this;};
+        unsigned int getTotalLeds() {return this->totalLeds;};
+        void animate() {
+            for (int i = 0 ; i < getRepetitions() ; i++) {
+                drawFrame();
+            }
+        }
 };
 
 #endif
