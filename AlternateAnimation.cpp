463f78c53d92caaacea8d0f736e8fb16f0123a27
Marco Federighi <m.federighi@nephila.it>
2014-12-22 15:17:13 +0100

    Add sin and cos based animations

--- AlternateAnimation.cpp
+++ AlternateAnimation.cpp
@@ -1,6 +1,9 @@
 #include "AlternateAnimation.h"
 #include <stdlib.h>
 #include <Arduino.h>
+#include <math.h> 
+
+#define PI 3.14159265
 
 void AlternateAnimation::drawFrame() {
     unsigned int leds = getTotalLeds();
@@ -22,4 +25,24 @@ void AlternateAnimation::drawFrame() {
         }  
     }   
     delay(1000);
+    for (int led = 0 ; led < leds ; led++) {
+        int y = floor(cos(led*PI/180));
+        if (y != 0) {
+            digitalWrite(led, HIGH);
+        }
+        else { 
+            digitalWrite(led, LOW);
+        }
+    }   
+    delay(500);
+    for (int led = 0 ; led < leds ; led++) {
+        int y = floor(cos(led*PI/180));
+        if (y != 0) {
+            digitalWrite(led, HIGH);
+        }
+        else { 
+            digitalWrite(led, LOW);
+        }
+    }   
+    delay(500);
 };
