ba014923df41a0932d07032040c90f99c0ac04ac
Andrea Stagi <stagi.andrea@gmail.com>
2014-12-22 21:18:02 +0100

    Add MoscowDisco animation

---
+++ MoscowDiscoAnimation.h
@@ -0,0 +1,11 @@
+#ifndef MOSCOW_DISCO_ANIMATION_CLASS
+#define MOSCOW_DISCO_ANIMATION_CLASS
+
+#include "Animation.h"
+
+class MoscowDiscoAnimation: public Animation {
+    public:
+        void drawFrame();
+};
+
+#endif
