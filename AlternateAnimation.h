b0da4fcf4cb4730b154f5c08e39632563d03413c
Leonardo Cavallucci <l.cavallucci@nephila.it>
2014-12-22 14:36:25 +0100

    Add alternate led animation

---
+++ AlternateAnimation.h
@@ -0,0 +1,11 @@
+#ifndef BLINK_ANIMATION_CLASS
+#define BLINK_ANIMATION_CLASS
+
+#include "Animation.h"
+
+class AlternateAnimation: public Animation {
+    public:
+        void drawFrame();
+};
+
+#endif
