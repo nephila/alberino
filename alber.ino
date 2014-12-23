22bfed3bb46bbbebc41da62c9b729df4c34923fe
Marco Federighi <m.federighi@nephila.it>
2014-12-23 14:23:15 +0100

    Add random animation

--- alber.ino
+++ alber.ino
@@ -12,6 +12,8 @@
 #include "JingleAnimation.h"
 #include "MoscowDiscoAnimation.h"
 #include "LadderAnimation.h"
+#include "RandomMathAnimation.h"
+
 
 unsigned int TOTAL_LEDS = 14;
 SlimArray<Animation*> animations;
@@ -26,6 +28,7 @@ void setup() {
     animations.insert((new LadderAnimation())->setTotalLeds(TOTAL_LEDS)->setRepetitions(4));
     animations.insert((new BlinkAnimation())->setTotalLeds(TOTAL_LEDS)->setRepetitions(2));
     animations.insert((new AlternateAnimation())->setTotalLeds(TOTAL_LEDS)->setRepetitions(4));
+    animations.insert((new RandomMathAnimation())->setTotalLeds(TOTAL_LEDS)->setRepetitions(2));
 }
 
 void cleanup() {
